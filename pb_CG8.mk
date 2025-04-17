810cec1ad6a5be6d125e4c41bbf9690ba9d63fbc
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-04-17 21:33:40 +0100

    Update pb_CG8.mk

--- pb_CG8.mk
+++ pb_CG8.mk
@@ -18,12 +18,15 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from CG8 device
-$(call inherit-product, device/tecno/CG8/device.mk)
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Inherit some common TWRP stuff.
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common PBRP stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
@@ -33,3 +36,5 @@ PRODUCT_BRAND := Tecno
 PRODUCT_MODEL := Camon 17 Pro
 PRODUCT_MANUFACTURER := Tecno
 PRODUCT_RELEASE_NAME := Tecno Camon 17 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
