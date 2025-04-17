09e2b6f48d3fc4744746b6b1d6458c8eee9faa2d
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-04-17 19:33:28 +0100

    Update and rename twrp_CG8.mk to pb_CG8.mk

--- pb_CG8.mk
+++ pb_CG8.mk
@@ -27,11 +27,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, device/tecno/CG8/device.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := CG8
-PRODUCT_NAME := twrp_CG8
+PRODUCT_NAME := pb_CG8
 PRODUCT_BRAND := Tecno
 PRODUCT_MODEL := Camon 17 Pro
 PRODUCT_MANUFACTURER := Tecno
